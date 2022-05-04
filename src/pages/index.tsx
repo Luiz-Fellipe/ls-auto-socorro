3ba3a7e53c72181b70ad6e2949d93bbf34cb1f25
Luiz-Fellipe <devluizfellipe@gmail.com>
2022-05-04 12:20:59 -0300

    feature: create section what we transport

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@
 
 import { SectionAboutUs } from '~/components/SectionAboutUs';
 import { SectionHome } from '~/components/SectionHome';
+import { SectionWhatWeTransport } from '~/components/SectionWhatWeTransport';
 
 export default function Home() {
   return (
     <main>
       <SectionHome />
       <SectionAboutUs />
+      <SectionWhatWeTransport />
     </main>
   );
 }
